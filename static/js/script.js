b310b54cf8a1f914b67f83f0af533f5f2a54304a
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-03-10 01:17:15 +0100

    rozdělení html + průběžné sčítání produktů

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,30 @@
-function setValue(from, to) {
-    var toElement = document.getElementById(to);
-    var fromElement = document.getElementById(from);
-    toElement.innerHTML = fromElement.value * 100;
+function calculatePrice() {
+    var quantity = document.getElementById("quantity");
+    var selectedProduct = document.getElementById("product");
+    var subtotal = document.getElementById("subtotal");
+    var calculation = document.getElementById("calculation");
+
+    var products = document.getElementById("products");
+    var productsList = JSON.parse(products.textContent)
+
+    for(var i = 0, size = productsList.length; i < size; i++) {
+        var item = productsList[i];
+        var q = quantity.value;
+        if (item["id"] == selectedProduct.value && q) {
+            var price = item["price"];
+            subtotal.innerHTML = price * q;
+            calculation.innerHTML = `(${price} CZK * ${q} pcs)`;
+            break;
+        }
+    }
 };
 
 var quantity = document.getElementById("quantity");
 quantity.onchange = function () {
-    setValue("quantity", "result");
+    calculatePrice();
 };
 
-window.onload = function () {
-    setValue("quantity", "result");
+var selectedProduct = document.getElementById("product");
+selectedProduct.onchange = function () {
+    calculatePrice();
 };
